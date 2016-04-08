bed903216c83955f1d32bf81fe308bf505ea5319
Danny Libin <omegasol11@gmail.com>
2016-04-08 16:05:11 -0500

    finished

--- script.js
+++ script.js
@@ -2,17 +2,26 @@
 require('./styles.css');
 const d3 = require('d3');
 const axios = require('axios');
+const _ = require('lodash');
 
 const w = 1200;
 const h = 600;
 
+let mouse = {x: 0, y: 0}
+
 window.onload = init();
+window.onmousemove = updateMouse;
 
 function init() {
 	axios.get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json')
 		.then(res => {
 			plotData(res.data);
 		});
+}
+
+function updateMouse(e) {
+	mouse.x = e.pageX;
+	mouse.y = e.pageY;
 }
 
 function plotData(jsonData) {
@@ -41,11 +50,30 @@ function plotData(jsonData) {
 					.domain(d3.range(1, 13))
 					.rangeRoundBands([0, h], 0.01);
 	
+	let xAxisTicks = _.sortedUniq(dataset.map(month => month['year'])).filter(num => num % 10 === 0);
+	let xAxis = d3.svg.axis().scale(xScale).orient('bottom').tickValues(xAxisTicks).tickFormat(d3.format('04d'));
+	
+	let yAxisTicks = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+						'August', 'September', 'October', 'November', 'December'];
+	let yAxis = d3.svg.axis().scale(yScale).orient('left').tickFormat(d => yAxisTicks[d - 1]);
+	
 	let svg = d3.select('#chart')
 				.append('svg')
-				.attr('width', w)
-				.attr('height', h);
-	console.log(dataset.length);
+				.attr('width', w + 20)
+				.attr('height', h)
+				.style('padding', 20 + 'px')
+				.style('padding-left', 70 + 'px');
+				
+	svg.append('g')
+		.attr('class', 'x axis')
+		.attr('transform', `translate(0, ${h})`)
+		.call(xAxis);
+		
+	svg.append('g')
+		.attr('class', 'y axis')
+		.attr('transform', 'translate(-1, 0)')
+		.call(yAxis);
+	
 	svg.selectAll('rect')
 		.data(dataset)
 		.enter()
@@ -58,34 +86,63 @@ function plotData(jsonData) {
 			let avgMonthTemp = baseTemp + d['variance'];
 			let cellCol = getCellColor(avgMonthTemp);
 			return cellCol;
-		});
+		})
+		.on('mouseover', d => {
+			let tooltip = d3.select('#tooltip');
+			tooltip.classed('hidden', false);
+			let tooltipDoc = document.getElementById('tooltip');
+			let tooltipH = tooltipDoc.clientHeight;
+			let tooltipW = tooltipDoc.clientWidth;
+			tooltip
+				.style('left', (mouse.x - tooltipW/2) + 'px')
+				.style('top', (mouse.y - tooltipH) + 'px')
+				.select('#month')
+				.text(yAxisTicks[d['month'] - 1]);
+			tooltip
+				.select('#year')
+				.text(d['year']);
+			tooltip
+				.select('#temperature')
+				.text(_.round(baseTemp + d['variance'], 3));
+			tooltip
+				.select('#delta')
+				.text(d['variance']);
+		})
+		.on('mouseout', () => d3.select('#tooltip').classed('hidden', true));
+		
+	// Legend
+	let legendData =  d3.range(baseTemp + d3.max(dataset, d => d['variance']));
+	let legendCellWidth = 30;
+	d3.select('#legend')
+		.append('svg')
+		.attr('width', legendData.length * legendCellWidth)
+		.attr('height', 20)
+		.selectAll('rect')
+		.data(legendData)
+		.enter()
+		.append('rect')
+		.attr('x', (d, i) => i * legendCellWidth + legendCellWidth)
+		.attr('height', 30)
+		.attr('width', legendCellWidth)
+		.attr('fill', d => getCellColor(d));
 }
 
 function getCellColor(avgMonthTemp) {
-	/*let maxColor = 255;
-	let maxTemp = 13;
-	let minTemp = 0;
-	let redRatio = avgMonthTemp / maxTemp;
-	let greenRatio = Math.abs( (avgMonthTemp - maxTemp) ) / (maxTemp / 2);
-	let blueRatio = (maxTemp - avgMonthTemp) / maxTemp;
-	let red = Math.floor(maxColor * redRatio);
-	let green = Math.floor(maxColor * greenRatio);
-	let blue = Math.floor(maxColor * blueRatio);*/
 	let coldTemp = 260;
-	let skipPoint = 155;
-	let skipAmount = 85;
+	let skipPoint = 180;
+	let skipAmount = 90;
 	let hotTemp = 0;
 	let maxDataTemp = 13;
 	
 	let heatRatio = avgMonthTemp / maxDataTemp; // The higher the avgTemp the higher the ratio
-	let colorTemp = Math.floor(coldTemp - ((coldTemp - 80) * heatRatio));
+	let colorTemp = Math.floor(coldTemp - ((coldTemp - (skipPoint - skipAmount)) * heatRatio));
 	let alpha = 1;
 	if (colorTemp < skipPoint && colorTemp > (skipPoint - skipAmount)) {
 		colorTemp -= skipAmount;
 	}
 	
 	if (colorTemp > 120 && colorTemp < 200) {
-		alpha = 0.3;
+		alpha = 0.9;
 	}
 	
 	return `hsla(${colorTemp}, 100%, 50%, ${alpha})`;
